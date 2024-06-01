7441f3037d3e3d6bd4b963f8206a3d820e864b70
Jhonatan <jhonatan.tamayom@udea.edu.co>
2024-06-01 00:00:48 -0500

    Lógica para mostrar la transmisión de la cámara en vivo

--- securityLab.py
+++ securityLab.py
@@ -1,13 +1,17 @@
+import cv2
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.videoplayer import VideoPlayer
+from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+
+from VideoStream import VideoStream
 
 class InicioScreen(Screen):
     def __init__(self, **kwargs):
@@ -15,26 +19,26 @@ class InicioScreen(Screen):
         window = GridLayout()
         window.cols = 1
         window.size_hint = (0.6, 0.7)
-        window.pos_hint = {"center_x": 0.5, "center_y":0.5}
+        window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
         btn_tabla = Button(
             text="Sensores",
-            size_hint= (1, 0.5),
-            bold= True,
-            background_color ='#76D7C4',
-            background_normal = '',
-            color = '#000000',
-            font_size = '24sp',
-            )
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#76D7C4',
+            background_normal='',
+            color='#000000',
+            font_size='24sp',
+        )
         spacer = Widget(size_hint_y=None, height=20)
         btn_video = Button(
             text="Cámara",
-            size_hint= (1, 0.5),
-            bold= True,
-            background_color ='#76D7C4',
-            background_normal = '',
-            color = '#000000',
-            font_size = '24sp',
-            )
+            size_hint=(1, 0.5),
+            bold=True,
+            background_color='#76D7C4',
+            background_normal='',
+            color='#000000',
+            font_size='24sp',
+        )
         window.add_widget(btn_tabla)
         window.add_widget(spacer)
         window.add_widget(btn_video)
@@ -48,13 +52,12 @@ class InicioScreen(Screen):
 
     def cambiar_a_video(self, *args):
         self.manager.current = 'video'
-        
+
 
 class TablaScreen(Screen):
     def __init__(self, **kwargs):
         super(TablaScreen, self).__init__(**kwargs)
-        
-        # self.sensors = ["Humo", "Sísmico"]
+
         self.sensors = {
             "humo": {
                 "name": "Humo",
@@ -69,30 +72,30 @@ class TablaScreen(Screen):
                 "labels": []
             }
         }
+
         # Layout principal con márgenes
         main_layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
-        
+
         # Título
         titulo = Label(text="Niveles de Sensores", font_size='20sp', size_hint=(1, 0.1))
-        
+
         # Layout para la tabla
         table_layout = GridLayout(cols=3, size_hint=(1, 0.8))
-        
+
         # Encabezados
         headers = ["Sensores", "Nivel", "Estado"]
         for header in headers:
             table_layout.add_widget(Label(text=header, bold=True))
-        
+
         # Filas de datos
-        # self.labels = []
         for v in self.sensors.values():
             table_layout.add_widget(Label(text=v['name']))
             valor_label = Label(text=str(v['value']))  # Nivel inicial
-            estado_label = Label(text='Uknown')  # Estado inicial
+            estado_label = Label(text='Unknown')  # Estado inicial
             table_layout.add_widget(valor_label)
             table_layout.add_widget(estado_label)
             v['labels'] = [valor_label, estado_label]
-        
+
         # Añadir widgets al layout principal
         main_layout.add_widget(titulo)
         main_layout.add_widget(table_layout)
@@ -147,7 +150,7 @@ class TablaScreen(Screen):
 
     def activar_alarmas(self, *args):
         print("¡Alarmas activadas!")
-        
+
 
 class VideoScreen(Screen):
     def __init__(self, **kwargs):
@@ -155,15 +158,15 @@ class VideoScreen(Screen):
         
         # Layout principal con márgenes
         main_layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
-        
+
         # Título
         titulo = Label(text="Cámara en vivo", font_size='20sp', size_hint=(1, 0.1))
-        
+
         # Layout para el video
         video_layout = BoxLayout(size_hint=(1, 0.8))
-        video = VideoPlayer(source='path_to_your_video.mp4', size_hint=(1, 1))
-        video_layout.add_widget(video)
-        
+        self.video_image = Image()
+        video_layout.add_widget(self.video_image)
+
         # Añadir widgets al layout principal
         main_layout.add_widget(titulo)
         main_layout.add_widget(video_layout)
@@ -202,13 +205,29 @@ class VideoScreen(Screen):
         btn_volver.bind(on_press=self.cambiar_a_inicio)
         btn_alarmas.bind(on_press=self.activar_alarmas)
 
+    def on_enter(self, *args):
+        self.video_stream = VideoStream(src=0).start()
+        Clock.schedule_interval(self.update_video, 1.0 / 30.0)  # 30 FPS
+
+    def update_video(self, dt):
+        frame = self.video_stream.read()
+        if frame is not None:
+            buf = cv2.flip(frame, 0).tostring()
+            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.video_image.texture = image_texture
+
+    def on_leave(self, *args):
+        Clock.unschedule(self.update_video)
+        self.video_stream.stop()
+
     def cambiar_a_inicio(self, *args):
         self.manager.current = 'inicio'
 
     def activar_alarmas(self, *args):
         print("¡Alarmas activadas!")
-        
-        
+
+
 class SecurityLab(App):
     def build(self):
         sm = ScreenManager()
@@ -216,6 +235,12 @@ class SecurityLab(App):
         sm.add_widget(TablaScreen(name='tabla'))
         sm.add_widget(VideoScreen(name='video'))
         return sm
+
+    def on_stop(self):
+        # Asegúrate de que todas las transmisiones se detengan cuando la aplicación se cierra
+        for screen in self.root.screens:
+            if isinstance(screen, VideoScreen):
+                screen.video_stream.stop()
 
 if __name__ == '__main__':
     SecurityLab().run()
