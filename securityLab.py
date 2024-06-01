776b6626f6cc969a39e3ab64e366e47ad778b9e8
Jeison David Moncayo Marzola <desarrollo5ude@udea.edu.co>
2024-06-01 13:40:30 -0500

    Conexion mqtt

--- securityLab.py
+++ securityLab.py
@@ -8,13 +8,24 @@ from kivy.uix.videoplayer import VideoPlayer
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
+from datetime import datetime, timedelta
 
 import paho.mqtt.client as mqttc
 from paho.mqtt import publish
 from _thread import start_new_thread
 
-BROKER_URL = "192.168.1.4"
+BROKER_URL = "192.168.192.4"
 BROKER_PORT = 1883
+
+class MyData:
+    activatedAlarms = 0
+    
+    @staticmethod
+    def getBtnAlarmsText():
+        if MyData.activatedAlarms == 1:
+            return "Apagar alarmas"
+        return "Encender alarmas"
+        
 
 class Publisher:
     @staticmethod
@@ -48,14 +59,18 @@ class Listener:
             topic = v['topic']
             print("Conectado ", str(rc), topic)
             client.subscribe(topic)
+        client.subscribe("lab/alarms")
 
 
     def on_message(self, client, userdata, msg):
         print("Mensaje recibido")
         print("Message: {}".format(msg.payload.decode("utf-8")))
         print(f'Topic: {msg.topic}')
-        # self.observador.procesarMensajeLuz(msg.payload.decode("utf-8"))
         self.observador.procesarDato(msg.topic, msg.payload.decode("utf-8"))
+
+
+
+publisher = Publisher()
 
 class InicioScreen(Screen):
     def __init__(self, **kwargs):
@@ -73,7 +88,7 @@ class InicioScreen(Screen):
             color = '#000000',
             font_size = '24sp',
             )
-        spacer = Widget(size_hint_y=None, height=20)
+        spacer = Widget(size_hint_y=None, height=10)
         btn_video = Button(
             text="Cámara",
             size_hint= (1, 0.5),
@@ -101,39 +116,46 @@ class InicioScreen(Screen):
 class TablaScreen(Screen):
     def __init__(self, **kwargs):
         super(TablaScreen, self).__init__(**kwargs)
-        
         # self.sensors = ["Humo", "Sísmico"]
+        dt_now = datetime.now()
+        delta = timedelta(seconds=15)
         self.sensors = {
-            "humo": {
+            "smoke": {
                 "name": "Humo",
-                "topic": "lab/sensors/humo",
-                "value": 0,
-                "state": 0,
+                "topic": "lab/sensors/smoke",
+                "last_time": dt_now-delta,
+                "value": -1,
+                "state": 2,
+                "up_limit": 1,
+                # "down_limit": 20,
                 "labels": []
             },
             "seism": {
                 "name": "Sísmico",
                 "topic": "lab/sensors/seism",
-                "value": 0,
-                "state": 0,
+                "last_time": dt_now-delta,
+                "value": -1,
+                "state": 2,
+                "up_limit": 1,
+                # "down_limit": 20,
                 "labels": []
             }
         }
         # Layout principal con márgenes
         
         try:
-            listener = Listener(self, self.sensors)
-            start_new_thread(listener.start, ())
+            self.listener = Listener(self, self.sensors)
+            start_new_thread(self.listener.start, ())
         except:
             print("Error al iniciar el listener")
         
-        main_layout = BoxLayout(orientation='vertical', padding=20, spacing=10)
+        main_layout = BoxLayout(orientation='vertical', padding=10, spacing=5)
         
         # Título
         titulo = Label(text="Niveles de Sensores", font_size='20sp', size_hint=(1, 0.1))
         
         # Layout para la tabla
-        table_layout = GridLayout(cols=3, size_hint=(1, 0.8))
+        table_layout = GridLayout(cols=3, size_hint=(1, 0.7))
         
         # Encabezados
         headers = ["Sensores", "Nivel", "Estado"]
@@ -144,8 +166,8 @@ class TablaScreen(Screen):
         # self.labels = []
         for v in self.sensors.values():
             table_layout.add_widget(Label(text=v['name']))
-            valor_label = Label(text=str(v['value']))  # Nivel inicial
-            estado_label = Label(text='Uknown')  # Estado inicial
+            valor_label = Label(text=self.getValue(v))  # Nivel inicial
+            estado_label = Label(text=self.getTextState(v['state']))  # Estado inicial
             table_layout.add_widget(valor_label)
             table_layout.add_widget(estado_label)
             v['labels'] = [valor_label, estado_label]
@@ -164,12 +186,12 @@ class TablaScreen(Screen):
             background_color='#76D7C4',
             background_normal='',
             color='#000000',
-            pos_hint={"x": 0.02, "y": 0.02}
+            pos_hint={"x": 0.02, "y": 0.02},
         )
 
         # Botón de activar alarmas
-        btn_alarmas = Button(
-            text="Activar Alarmas",
+        self.btn_alarmas = Button(
+            text=MyData.getBtnAlarmsText(),
             size_hint=(None, None),
             size=(140, 55),
             background_color='#E74C3C',
@@ -179,25 +201,28 @@ class TablaScreen(Screen):
         )
 
         float_layout.add_widget(btn_volver)
-        float_layout.add_widget(btn_alarmas)
+        float_layout.add_widget(self.btn_alarmas)
 
         # Añadir layouts al screen
         self.add_widget(main_layout)
         self.add_widget(float_layout)
 
         btn_volver.bind(on_press=self.cambiar_a_inicio)
-        btn_alarmas.bind(on_press=self.activar_alarmas)
+        self.btn_alarmas.bind(on_press=self.handleAlarms)
 
         # Simulación de actualización de datos cada 2 segundos
-        Clock.schedule_interval(self.actualizar_datos, 2)
+        Clock.schedule_interval(self.actualizar_datos, 1)
         
     def procesarDato(self, topic, message):
+        if topic == "lab/alarms":
+            MyData.activatedAlarms = float(message)
+            return
         flag = False
         for v in self.sensors.values():
             if v['topic'] == topic:
                 try:
                     v['value'] = float(message)
-                    v['state'] = 1 if float(message) > 50 else 0
+                    v['last_time'] = datetime.now()
                     flag = True
                 except:
                     print("Error al procesar el mensaje")
@@ -207,21 +232,67 @@ class TablaScreen(Screen):
             print("Procesado")
         else:
             print("Mensage No procesado")
+            
+    def getValue(self, sensor):
+        if sensor['state'] == 2:
+            return "Uknown"
+        return str(sensor['value'])
+            
+    def getTextState(slef, state):
+        if state == 0:
+            return "Apagado"
+        if state == 1:
+            return "Normal"
+        if state == 2:
+            return "Uknown"
+        return "Alarm!!!"
+    
+    def calculateState(self, sensor):
+        now = datetime.now()
+        diff = now - sensor['last_time']
+        if diff.total_seconds() > 10:
+            sensor['state'] = 2
+            return
+        sensor['state'] = 1
+        if 'up_limit' in sensor:
+            if sensor['value'] >= sensor['up_limit']:
+                sensor['state'] = -1
+                publisher.send_message('1', 'lab/alarms')
+                MyData.activatedAlarms = 1
+                return
+        if 'down_limit' in sensor:
+            if sensor['value'] <= sensor['down_limit']:
+                sensor['state'] = -1
+                publisher.send_message('1', 'lab/alarms')
+                MyData.activatedAlarms = 1
+                return
+        
 
     def actualizar_datos(self, dt):
         # Esta función simula la actualización de los datos
         # import random
-        
         for v in self.sensors.values():
+            self.calculateState(v)
             labels = v['labels']
-            labels[0].text = str(v['value'])
-            labels[1].text = "Alerta" if v['state'] == 1 else "Normal"
+            labels[0].text = self.getValue(v)
+            labels[1].text = self.getTextState(v['state'])
+        
+        self.btn_alarmas.text = MyData.getBtnAlarmsText()
 
     def cambiar_a_inicio(self, *args):
         self.manager.current = 'inicio'
 
-    def activar_alarmas(self, *args):
-        print("¡Alarmas activadas!")
+    def handleAlarms(self, *args):
+        if MyData.activatedAlarms == 0:
+            print("¡Activando alarmas!")
+            publisher.send_message('1', 'lab/alarms')
+            MyData.activatedAlarms = 1
+            print("Eviado 1 a lab/alarm")
+        else:
+            print("!Apagando alarmas!")
+            publisher.send_message('0', 'lab/alarms')
+            MyData.activatedAlarms = 0
+            print("Eviado 0 a lab/alarm")
         
 
 class VideoScreen(Screen):
@@ -257,8 +328,8 @@ class VideoScreen(Screen):
         )
 
         # Botón de activar alarmas
-        btn_alarmas = Button(
-            text="Activar Alarmas",
+        self.btn_alarmas = Button(
+            text=MyData.getBtnAlarmsText(),
             size_hint=(None, None),
             size=(140, 55),
             background_color='#E74C3C',
@@ -268,20 +339,34 @@ class VideoScreen(Screen):
         )
 
         float_layout.add_widget(btn_volver)
-        float_layout.add_widget(btn_alarmas)
+        float_layout.add_widget(self.btn_alarmas)
 
         # Añadir layouts al screen
         self.add_widget(main_layout)
         self.add_widget(float_layout)
 
         btn_volver.bind(on_press=self.cambiar_a_inicio)
-        btn_alarmas.bind(on_press=self.activar_alarmas)
+        self.btn_alarmas.bind(on_press=self.handleAlarms)
+        
+        Clock.schedule_interval(self.updateData, 2)
+        
+    def updateData(self, *args):
+        self.btn_alarmas.text = MyData.getBtnAlarmsText()
 
     def cambiar_a_inicio(self, *args):
         self.manager.current = 'inicio'
 
-    def activar_alarmas(self, *args):
-        print("¡Alarmas activadas!")
+    def handleAlarms(self, *args):
+        if MyData.activatedAlarms == 0:
+            print("¡Activando alarmas!")
+            publisher.send_message('1', 'lab/alarms')
+            MyData.activatedAlarms = 1
+            print("Eviado 1 a lab/alarm")
+        else:
+            print("!Apagando alarmas!")
+            publisher.send_message('0', 'lab/alarms')
+            MyData.activatedAlarms = 0
+            print("Eviado 0 a lab/alarm")
         
         
 class SecurityLab(App):
